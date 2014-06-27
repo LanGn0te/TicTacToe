42881a38adfacba54eb6c6533d8dc69b9cd40cca
LanGn0te <contato.langn0te@gmail.com>
2014-06-26 21:34:30 -0300

    First Commit
    The main code

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Usuario
+ *
+ * Created on 26 de Junho de 2014, 21:02
+ */
+//Includes da vida
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
