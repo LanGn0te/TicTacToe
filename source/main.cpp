732b115b5c393a7831f3dadb05aa9ca9ee10a9d5
Isaac Elias <contato.langn0te@gmail.com>
2014-08-22 19:53:51 -0300

    Junta os dois headers e comeca implementar o jogo

--- source/main.cpp
+++ source/main.cpp
@@ -5,37 +5,62 @@
  * Created on 26 de Junho de 2014, 21:02
  */
 
-/*
- * Tasks
- * 
- * * Terminar metodo de movimentacao impossivel
- * * que sera usado como base paea movimentacao normal
- * 
- */
-
-#include "tictactoeipmt.h"
+#include "tictactoe.h"
 
 int main() {
-    
-    /* Sessao de boas vindas! Eeeee! */
-    
-    int optEsc;
-    cout << endl << endl << endl << endl;
-    cout << "      Bem vindo ao TicTacToe!";
-    cout << endl << endl;
-    cout << "  1 - Jogar" << endl;
-    cout << "  0 - Sair" << endl << endl;
-    cin >> optEsc;
-    
-    /* Lugar da jogatina */
-    
-    switch(optEsc) {
-        case 1: //Caso o usuario escolha jogar
-           break; 
-            
-        case 2:
-            break;
+
+    bool sair;
+    while (sair) {
+
+        /* Variaveis! */
+
+        int optEsc, numJog, tabP[9], player, aux;
+        numJog = 0;
+        tabP[1] = 0;
+        tabP[2] = 0;
+        tabP[3] = 0;
+        tabP[4] = 0;
+        tabP[5] = 0;
+        tabP[6] = 0;
+        tabP[7] = 0;
+        tabP[8] = 0;
+        tabP[9] = 0;
+
+        /* Sessao de boas vindas! Eeeee! */
+
+        cout << endl << endl << endl << endl;
+        cout << "      Bem vindo ao TicTacToe!";
+        cout << endl << endl;
+        cout << "  1 - Jogar" << endl;
+        cout << "  0 - Sair" << endl << endl;
+        cout << "Sua escolha: ";
+        cin >> optEsc;
+
+        /* Lugar da jogatina */
+
+        switch (optEsc) {
+            case 1: //Caso o usuario escolha jogar
+                while (checkWinner(tabP[1], tabP[2], tabP[3], tabP[4], tabP[5], tabP[6], tabP[7], tabP[8], tabP[9])) {
+                    numJog++;
+                    aux = numJog % 2;
+                    if (aux = 1)
+                        player = 1;
+                    else if (aux = 0)
+                        player = 2;
+                    cout << endl << endl;
+                    showTabIns();
+                    cout << endl;
+                    showTab(tabP[1], tabP[2], tabP[3], tabP[4], tabP[5], tabP[6], tabP[7], tabP[8], tabP[9]);
+                    cout << endl << "Player " << player << ": ";
+                    cin >> optEsc;
+                }
+                break;
+
+            case 2: //Caso ele queira virar presunto
+                sair = true;
+                break;
+        }
     }
-    
+    cout << endl << "Bye";
     return EXIT_SUCCESS;
 }
--- source/tictactoe.h
+++ source/tictactoe.h
@@ -71,3 +71,87 @@ int checkWinner(int pl1,int pl2,int pl3,int pl4,int pl5,int pl6,int pl7,int pl8,
     else
         return 0;
 }
+
+void showTabIns() {                 //Completed
+    cout << "1|2|3" << endl;        //This void function just shows the
+    cout << "4|5|6" << endl;        //numbers of the places for be
+    cout << "7|8|9" << endl;        //easier to the user locate the place he wants
+}
+
+void showTab(int pl1,int pl2,int pl3,int pl4,int pl5,int pl6,int pl7,int pl8,int pl9) {
+    string plC[9];
+    //If for plC[1]
+    if(pl1 == 0)
+        plC[1] = " ";
+    else if(pl1 == 1)
+        plC[1] = "X";
+    else
+        plC[1] = "O";
+    
+    if(pl2 == 0)
+        plC[2] = " ";
+    else if(pl2 == 1)
+        plC[2] = "X";
+    else
+        plC[2] = "O";
+    
+    if(pl3 == 0)
+        plC[3] = " ";
+    else if(pl3 == 1)
+        plC[3] = "X";
+    else
+        plC[3] = "O";
+    
+    if(pl4 == 0)
+        plC[4] = " ";
+    else if(pl4 == 1)
+        plC[4] = "X";
+    else
+        plC[4] = "O";
+    
+    if(pl5 == 0)
+        plC[5] = " ";
+    else if(pl5 == 1)
+        plC[5] = "X";
+    else
+        plC[5] = "O";
+    
+    if(pl6 == 0)
+        plC[6] = " ";
+    else if(pl6 == 1)
+        plC[6] = "X";
+    else
+        plC[6] = "O";
+    
+    if(pl7 == 0)
+        plC[7] = " ";
+    else if(pl7 == 1)
+        plC[7] = "X";
+    else
+        plC[7] = "O";
+    
+    if(pl8 == 0)
+        plC[8] = " ";
+    else if(pl8 == 1)
+        plC[8] = "X";
+    else
+        plC[8] = "O";
+    
+    if(pl9 == 0)
+        plC[9] = " ";
+    else if(pl9 == 1)
+        plC[9] = "X";
+    else
+        plC[9] = "O";
+    
+    cout << endl << endl;                               //Skip 2 lines to make it more handsome
+    showTabIns();                                       //Show the place numbers to make easier to our user to find the place that he wants
+    cout << endl << endl;                               //Skip 2 lines to make it more handsome
+    cout << plC[1] << bar << plC[2] << bar << plC[3];   //Shows the first line
+    cout << endl;                                       //Skips to next line
+    cout << plC[4] << bar << plC[5] << bar << plC[6];   //Shows the second line
+    cout << endl;                                       //Skips to next line
+    cout << plC[7] << bar << plC[8] << bar << plC[9];   //Show the thirth
+    cout << endl;                                       //Skips 2 lines to make it more handsome
+    
+}
